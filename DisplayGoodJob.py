99353c66475d9241321cf1e8c1163a0749b78623
sand codes <codessand@gmail.com>
2026-05-03 15:48:51 +0200

    Added thumbs up image and file for display

---
+++ DisplayGoodJob.py
@@ -0,0 +1,21 @@
+import sys
+import os
+from PIL import Image
+from waveshare_epd import epd4in2_V2
+
+try:
+    epd = epd4in2_V2.EPD()
+    epd.init()
+    epd.Clear()
+
+    Himage = Image.open('anime.jpg')
+
+    Himage = Himage.resize((epd.width, epd.height))
+    
+    Himage = Himage.convert('1')
+
+    epd.display(epd.getbuffer(Himage))
+    epd.sleep()
+
+except IOError as e:
+    print(e)
